f4ecfaf834f607ca73ea8486ef4ce37a29e2aaf1
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-16 13:20:03 +0000

    Added the filtering of attributes

--- Selector.js
+++ Selector.js
@@ -11,6 +11,10 @@ window.Selector = function(path, context) {
 			// Return all elements
 			return context.all || context.getElementsByTagName('*');
 		},
+		attribute: function(e, name){
+			var attr = e.attributes.getNamedItem(name);
+			return attr && attr.value;
+		},
 		filterTag: function(elements, tagName, not) {
 			// Set up the array to be returned
 			var filtered = new Array();
@@ -36,7 +40,25 @@ window.Selector = function(path, context) {
 			
 			// Loop through all passed elements
 			for(var i = 0; i < elements.length; i++) {
+				// Compare classes
 				if((not) ? !classRegexp.test(elements[i].className) : classRegexp.test(elements[i].className)) {
+					// Push to the filtered array
+					filtered.push(elements[i]);
+				}
+			}
+			
+			// Return the filtered array
+			return filtered;
+		},
+		filterAttribute: function(elements, attributeName, attributeValue, not){
+			// Set up array to be returned
+			var filtered = new Array();
+			
+			// Loop through all passed elements
+			for(var i = 0; i < elements.length; i++){
+				// Compare attributes
+				if((not) ? this.attribute(elements[i], attributeName) != attributeValue : this.attribute(elements[i], attributeName) == attributeValue) {
+					// Push to the filtered array
 					filtered.push(elements[i]);
 				}
 			}
