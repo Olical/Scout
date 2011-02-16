64b9da1799a9990068c774a6ebd4cbcba9a6c20e
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-16 12:51:40 +0000

    Added a massive comment containing a few methods

--- Selector.js
+++ Selector.js
@@ -1,3 +1,67 @@
 window.Selector = function(path, context) {
 	
 };
+
+/*
+
+DOMNodes.prototype = {
+
+       ofTag: function(nm){
+
+         var newList = [];
+
+          for(var i=0, l=this.nodes.length; i<l; i++){
+
+         if( this.nodes[i].tagName == nm.toUpperCase() ){
+
+            newList.push(this.nodes[i]);
+
+       }
+
+ }
+
+ return new DOMNodes(newList);
+
+ },
+
+  hasClass: function(clnm){
+
+      var newList = [];
+
+      var classRE = new RegExp("\\s?"+clnm+"\\s?");
+
+      for(var i=0, l=this.nodes.length; i<l; i++){
+
+      if( classRE.test(this.nodes[i].className) ){
+
+     newList.push(this.nodes[i]);
+
+     }
+
+ }
+
+      return new DOMNodes(newList);
+
+ },
+
+      withAttr: function(nm, val){
+
+      var newList = [];
+
+      for(var i=0, l=this.nodes.length; i<l; i++){
+
+     if( Selector.getAttr(this.nodes[i], nm) == val ){
+
+      newList.push(this.nodes[i]);
+
+     }
+
+ }
+
+   return new DOMNodes(newList);
+
+  }
+
+};
+
+*/
