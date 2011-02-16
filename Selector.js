6b33879983f04b855c3efa0aae5845dd9ebb09e7
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-16 13:11:20 +0000

    Added the filter class method

--- Selector.js
+++ Selector.js
@@ -11,16 +11,33 @@ window.Selector = function(path, context) {
 			// Return all elements
 			return context.all || context.getElementsByTagName('*');
 		},
-		filterTag: function(elements, tag, not) {
+		filterTag: function(elements, tagName, not) {
 			// Set up the array to be returned
 			var filtered = new Array();
 			
 			// Loop through all passed elements
 			for(var i = 0; i < elements.length; i++) {
 				// Compare tags
-				if((not) ? elements[i].tagName != tag.toUpperCase() : elements[i].tagName == tag.toUpperCase()) {
+				if((not) ? elements[i].tagName != tagName.toUpperCase() : elements[i].tagName == tagName.toUpperCase()) {
 					// Push to the filtered array
 					 filtered.push(elements[i]); 
+				}
+			}
+			
+			// Return the filtered array
+			return filtered;
+		},
+		filterClass: function(elements, className, not) {
+			// Set up array to be returned
+			var filtered = new Array();
+			
+			// Set up class regexp
+			var classRegexp = new RegExp("\\s?" + className + "\\s?");
+			
+			// Loop through all passed elements
+			for(var i = 0, i < elements; i++) {
+				if(classRegexp.test(elements[i].className)) {
+					filtered.push(elements[i]);
 				}
 			}
 			
